132ede5e3ec6e03137e95504802eeee47d9454d6
preeti <preetikushwaha0110@gmail.com>
2024-10-28 14:49:55 +0530

    added connection

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-const mongoURL='mongodb://localhost:27017/hotels'
+require('dotenv').config();
+const mongoURL=process.env.mongoURL
 
 mongoose.connect(mongoURL)
 

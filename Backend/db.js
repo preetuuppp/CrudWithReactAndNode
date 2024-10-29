117e2667e40f0fff75509e31a6a128a9bb11090d
preeti <preetikushwaha0110@gmail.com>
2024-10-29 11:16:02 +0530

    env added

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,6 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-// const mongoURL=process.env.mongoURL
-const mongoURL="mongodb://localhost:27017/hotels"
+const mongoURL=process.env.mongoURL
 mongoose.connect(mongoURL)
 
 const db= mongoose.connection;
